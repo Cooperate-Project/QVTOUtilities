7a24ce4277a7097c4e619a4f532d94ddf70fca66
Stephan Seifermann <seifermann@fzi.de>
2017-06-29 09:26:14 +0200

    Added operation to get all contents by looking for the root container

--- bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
+++ bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
@@ -3,11 +3,19 @@ package de.cooperateproject.qvtoutils.blackbox;
 import java.util.LinkedHashSet;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 import org.eclipse.ocl.util.CollectionUtil;
 
-public final class CooperateLibrary {
+/**
+ * Library for several utility operations related to the Cooperate project.
+ */
+public class CooperateLibrary {
 
+    /**
+     * Instantiates the library.
+     */
+    @SuppressWarnings("squid:S1118")
     public CooperateLibrary() {
         super();
     }
@@ -15,9 +23,19 @@ public final class CooperateLibrary {
     @SuppressWarnings("squid:S1319")
     @Operation(contextual=true)
     public static LinkedHashSet<EObject> getAllContents(Object rootObject) {
+        return getAllContents(rootObject, true);
+    }
+    
+    @SuppressWarnings("squid:S1319")
+    @Operation(contextual=true)
+    public static LinkedHashSet<EObject> getAllContents(Object rootObject, boolean forRoot) {
         LinkedHashSet<EObject> result = CollectionUtil.createNewOrderedSet();
         if (rootObject instanceof EObject) {
-            ((EObject) rootObject).eAllContents().forEachRemaining(result::add);
+            EObject typedRootObject = (EObject)rootObject;
+            if (forRoot) {
+                typedRootObject = EcoreUtil.getRootContainer(typedRootObject);
+            }
+            typedRootObject.eAllContents().forEachRemaining(result::add);
         }
         return result;
     }
