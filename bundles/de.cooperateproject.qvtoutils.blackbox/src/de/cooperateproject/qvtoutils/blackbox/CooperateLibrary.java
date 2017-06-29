63142e2c0e38da3ec647e20e6bc55eea2f098e59
Stephan Seifermann <seifermann@fzi.de>
2017-06-29 10:32:23 +0200

    Added logging.

--- bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
+++ bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
@@ -1,10 +1,13 @@
 package de.cooperateproject.qvtoutils.blackbox;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
+import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
+import org.eclipse.m2m.qvt.oml.util.IContext;
 import org.eclipse.ocl.util.CollectionUtil;
 
 /**
@@ -19,25 +22,30 @@ public class CooperateLibrary {
     public CooperateLibrary() {
         super();
     }
-    
+
     @SuppressWarnings("squid:S1319")
-    @Operation(contextual=true)
-    public static LinkedHashSet<EObject> getAllContents(Object rootObject) {
-        return getAllContents(rootObject, true);
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.QUERY,
+        description = "Determines all contained elements of the given object.")
+    public static LinkedHashSet<EObject> getAllContents(IContext executionContext, Object rootObject) {
+        return getAllContents(executionContext, rootObject, true);
     }
-    
+
     @SuppressWarnings("squid:S1319")
-    @Operation(contextual=true)
-    public static LinkedHashSet<EObject> getAllContents(Object rootObject, boolean forRoot) {
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.QUERY,
+        description = "Determines all elements of the containment tree, to which the given object belongs to.")
+    public static LinkedHashSet<EObject> getAllContents(IContext executionContext, Object rootObject, boolean forRoot) {
         LinkedHashSet<EObject> result = CollectionUtil.createNewOrderedSet();
         if (rootObject instanceof EObject) {
-            EObject typedRootObject = (EObject)rootObject;
+            EObject typedRootObject = (EObject) rootObject;
             if (forRoot) {
                 typedRootObject = EcoreUtil.getRootContainer(typedRootObject);
             }
             typedRootObject.eAllContents().forEachRemaining(result::add);
+        } else {
+            executionContext.getLog().log("Unsupported type for call to getAllContents()",
+                    Optional.ofNullable(rootObject).map(Object::getClass).orElseGet(null));
         }
         return result;
     }
-    
+
 }
