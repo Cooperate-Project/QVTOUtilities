9d17b88dae177ae2ae2e8cdb3ec33c2215e92b29
Stephan Seifermann <seifermann@fzi.de>
2017-06-30 16:57:40 +0200

    Added set and add helper operations for collections.

--- bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
+++ bundles/de.cooperateproject.qvtoutils.blackbox/src/de/cooperateproject/qvtoutils/blackbox/CooperateLibrary.java
@@ -1,9 +1,12 @@
 package de.cooperateproject.qvtoutils.blackbox;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
@@ -46,6 +49,50 @@ public class CooperateLibrary {
                     Optional.ofNullable(rootObject).map(Object::getClass).orElseGet(null));
         }
         return result;
+    }
+    
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.HELPER,
+            description = "Adds the given element to multi-valued feature of the given context element.")
+    public static void addToFeature(IContext executionContext, Object context, String featureName, Object element) {
+        addToFeature(executionContext, context, featureName, Arrays.asList(element));
+    }
+    
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.HELPER,
+            description = "Adds the given elements to multi-valued feature of the given context element.")
+    public static void addToFeature(IContext executionContext, Object context, String featureName, Collection<Object> elements) {
+        getFeatureCollection(context, featureName).addAll(elements);
+    }
+    
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.HELPER,
+            description = "Replaces existing elements with the given element in a multi-valued feature of the given context element.")
+    public static void setToFeature(IContext executionContext, Object context, String featureName, Object element) {
+        setToFeature(executionContext, context, featureName, Arrays.asList(element));
+    }
+    
+    @Operation(contextual = true, withExecutionContext = true, kind = Kind.HELPER,
+            description = "Replaces existing elements with the given elements in a multi-valued feature of the given context element.")
+    public static void setToFeature(IContext executionContext, Object context, String featureName, Collection<Object> elements) {
+        Collection<Object> values = getFeatureCollection(context, featureName);
+        values.clear();
+        values.addAll(elements);
+    }
+    
+    private static Collection<Object> getFeatureCollection(Object context, String featureName) {
+        if (!(context instanceof EObject)) {
+            throw new IllegalArgumentException(String.format("The given element %s is not an EObject.", context));
+        }
+        
+        EObject typedContext = (EObject)context;
+        Optional<EReference> possibleFeature = typedContext.eClass().getEAllReferences().stream().filter(f -> f.getName().equals(featureName)).findFirst();
+        if (!possibleFeature.map(EReference::isMany).orElse(false)) {
+            throw new IllegalArgumentException(String.format("The given feature %s is not a multi-valued reference.", featureName));
+        }
+        
+        Optional<Collection<Object>> values = possibleFeature.map(typedContext::eGet).filter(Collection.class::isInstance).map(Collection.class::cast);
+        if (!values.isPresent()) {
+            throw new IllegalStateException("The feature does not contain a collection.");
+        }
+        return values.get();
     }
 
 }
